5dd6701529dc396f7fa9cee704003c608ac5f92f
Alexsand74 <1974alexsand@gmail.com>
2024-09-02 17:40:55 +0300

    ......

---
+++ src/org/javaacademy/homework/homework1/ex2/RunnerEx2.java
@@ -0,0 +1,39 @@
+package org.javaacademy.homework.homework1.ex2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RunnerEx2 {
+    public static void start() {
+        //Задание №2 Гидрометцентр
+        //Создать две структуры данных, в которых будет статистика по дням (среднесуточная температура воздуха)
+        //Январь: 0,-1,-1,-2,-5,-6,-7,-8,-9,-10,-5,-5,-2,-7,-3,-1,-8,-9,-8,-8,-18,-20,-23,-24,-25,-9,-8,-7,-6,-5,-1
+        //Февраль: -8,-10,-12,-13,-15,-16,-12,-7,-8,-10,-10,-9,-8,-8,-8,-9,-10,-9,-5,-6,-8,-7,-8,-9,-6,-5,-3,-1
+        //Объединить данные за январь и февраль, посчитать среднесуточную температуру воздуха за 2 месяца.
+        //Запрещено использовать массивы.
+
+        ArrayList<Integer> january = new ArrayList<>(List.of(0, -1, -1, -2, -5, -6, -7, -8, -9, -10,
+                                                                       -5, -5, -2, -7, -3, -1, -8, -9, -8, -8, -18,
+                                                                       -20, -23, -24, -25, -9, -8, -7, -6, -5, -1));
+        ArrayList<Integer> february = new ArrayList<>(List.of(-8, -10, -12, -13, -15, -16, -12, -7, -8,
+                                                                       -10, -10, -9, -8, -8, -8, -9, -10, -9, -5,
+                                                                             -6, -8, -7, -8, -9, -6, -5, -3,  -1));
+
+        ArrayList<Integer> januaryAndFebruary = new ArrayList<>(january);
+        januaryAndFebruary.addAll(february);
+        Double averageDailyAirTemp = (double) sumTemperaturesAllDays(januaryAndFebruary) / januaryAndFebruary.size();
+        System.out.format("Cреднесуточную температуру воздуха за 2 месяца = %.3f \n", averageDailyAirTemp);
+
+    }
+
+    public static Integer sumTemperaturesAllDays(ArrayList<Integer> arrayListTemperatures) {
+        int sumOfTemperatures = 0;
+        for (Integer temperatureForOneDay : arrayListTemperatures) {
+            if (temperatureForOneDay == null) {
+                throw new  RuntimeException(" The data contains the value null ! ");
+            }
+            sumOfTemperatures += temperatureForOneDay;
+        }
+        return sumOfTemperatures;
+    }
+}
